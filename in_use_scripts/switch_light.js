115884535eee3e493f280170e4f3f30763c11a36
unknown <vedanshkt.sfc@gmail.com>
2015-03-10 22:51:57 +0530

    adding another file

---
+++ in_use_scripts/switch_light.js
@@ -0,0 +1,10 @@
+﻿#pragma strict
+
+
+function change_status(object : GameObject,state:int){
+	if (state ==0)
+		object.gameObject.SetActive(false);
+	else
+		object.gameObject.SetActive(true);
+
+}
